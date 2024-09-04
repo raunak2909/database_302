da7728f337922f3f3d95b8155b6111796b88ac13
raunakpurohit <raunak.wscubetech@gmail.com>
2024-09-04 21:57:45 +0530

    Add and fetch implemented

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -16,6 +16,12 @@ class DBHelper{
 
   Database? mDB;
 
+  ///
+  static final String TABLE_NOTE_NAME = "note";
+  static final String COLUMN_NOTE_ID = "note_id";
+  static final String COLUMN_NOTE_TITLE = "note_title";
+  static final String COLUMN_NOTE_DESC = "note_desc";
+
   Future<Database> getDB() async{
     if(mDB!=null){
       return mDB!;
@@ -34,9 +40,34 @@ class DBHelper{
     return openDatabase(dbPath, version: 1, onCreate: (db, version){
 
       /// create all your tables here
+      db.execute("create table $TABLE_NOTE_NAME ( $COLUMN_NOTE_ID integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text)");
 
     });
 
+  }
+
+  ///db functions (queries)
+  ///insert
+  Future<bool> addNote({required String title, required String desc}) async{
+
+    Database db = await getDB();
+
+    int rowsEffected = await db.insert(TABLE_NOTE_NAME, {
+      COLUMN_NOTE_TITLE : title,
+      COLUMN_NOTE_DESC : desc
+    });
+
+    return rowsEffected>0;
+
+  }
+
+  ///fetch
+  Future<List<Map<String, dynamic>>> getAllNotes() async{
+    var db = await getDB();
+
+    List<Map<String, dynamic>> mData = await db.query(TABLE_NOTE_NAME);
+
+    return mData;
   }
 
 
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,20 +2,56 @@ import 'package:database_302/db_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
+  State<HomePage> createState() => _HomePageState();
+}
 
-   //DBHelper dbHelper = DBHelper();
-   DBHelper dbHelper = DBHelper.getDB();
+class _HomePageState extends State<HomePage> {
+  DBHelper dbHelper = DBHelper.getInstance();
 
+  List<Map<String, dynamic>> allNotes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getMyNotes();
+  }
+
+  @override
+  Widget build(BuildContext context){
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
+      body: ListView.builder(
+        itemCount: allNotes.length,
+          itemBuilder: (_, index){
+        return ListTile(
+          title: Text(allNotes[index][DBHelper.COLUMN_NOTE_TITLE]),
+          subtitle: Text(allNotes[index][DBHelper.COLUMN_NOTE_DESC]),
+        );
+      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          bool check = await dbHelper.addNote(
+              title: "My note",
+              desc: "Do what you love or Love what you do!!");
+
+          if(check){
+            getMyNotes();
+          }
+        },
+        child: Icon(Icons.add),
+      ),
     );
+  }
 
+  void getMyNotes() async{
+    allNotes = await dbHelper.getAllNotes();
+    setState(() {
 
+    });
   }
 }
