2ec2c0ec0e13d1cdc1b865e1ef77163bcd44bcf5
raunakpurohit <raunak.wscubetech@gmail.com>
2024-09-06 22:11:27 +0530

    database all operations implemented with date included

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -21,14 +21,18 @@ class DBHelper{
   static final String COLUMN_NOTE_ID = "note_id";
   static final String COLUMN_NOTE_TITLE = "note_title";
   static final String COLUMN_NOTE_DESC = "note_desc";
+  static final String COLUMN_NOTE_CREATED_AT = "note_created_at";
 
   Future<Database> getDB() async{
-    if(mDB!=null){
+    mDB ??= await openDB();
+    return mDB!;
+
+    /*if(mDB!=null){
       return mDB!;
     } else {
       mDB = await openDB();
       return mDB!;
-    }
+    }*/
   }
 
   Future<Database> openDB() async {
@@ -40,7 +44,7 @@ class DBHelper{
     return openDatabase(dbPath, version: 1, onCreate: (db, version){
 
       /// create all your tables here
-      db.execute("create table $TABLE_NOTE_NAME ( $COLUMN_NOTE_ID integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text)");
+      db.execute("create table $TABLE_NOTE_NAME ( $COLUMN_NOTE_ID integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text, $COLUMN_NOTE_CREATED_AT text)");
 
     });
 
@@ -48,13 +52,14 @@ class DBHelper{
 
   ///db functions (queries)
   ///insert
-  Future<bool> addNote({required String title, required String desc}) async{
+  Future<bool> addNote({required String title, required String desc, required String createdAt}) async{
 
     Database db = await getDB();
 
     int rowsEffected = await db.insert(TABLE_NOTE_NAME, {
       COLUMN_NOTE_TITLE : title,
-      COLUMN_NOTE_DESC : desc
+      COLUMN_NOTE_DESC : desc,
+      COLUMN_NOTE_CREATED_AT: createdAt,
     });
 
     return rowsEffected>0;
@@ -71,16 +76,30 @@ class DBHelper{
   }
 
   ///update
-  Future<bool> updateNote({ required String updatedTitle, required String updatedDesc, required int id}) async{
+  Future<bool> updateNote({ required String updatedTitle, required String updatedDesc, required String updatedAt, required int id}) async{
     var db = await getDB();
 
     int rowsEffected = await db.update(TABLE_NOTE_NAME, {
       COLUMN_NOTE_TITLE : updatedTitle,
-      COLUMN_NOTE_DESC : updatedDesc
+      COLUMN_NOTE_DESC : updatedDesc,
+      COLUMN_NOTE_CREATED_AT : updatedAt
     }, where: "$COLUMN_NOTE_ID = $id");
 
     return rowsEffected>0;
   }
+
+  ///delete
+  Future<bool> deleteNote({required int id}) async{
+
+    var db = await getDB();
+
+    int rowsEffected = await db.delete(TABLE_NOTE_NAME, where: "$COLUMN_NOTE_ID = ?", whereArgs: ['$id']);
+
+    return rowsEffected>0;
+
+  }
+
+
 
 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:database_302/db_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,6 +14,8 @@ class _HomePageState extends State<HomePage> {
   TextEditingController descController = TextEditingController();
 
   List<Map<String, dynamic>> allNotes = [];
+  
+  DateFormat mFormat = DateFormat.yMd();
 
   @override
   void initState() {
@@ -31,28 +34,36 @@ class _HomePageState extends State<HomePage> {
               itemCount: allNotes.length,
               itemBuilder: (_, index) {
                 return ListTile(
-                  title: Text(allNotes[index][DBHelper.COLUMN_NOTE_TITLE]),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(allNotes[index][DBHelper.COLUMN_NOTE_TITLE]),
+                      Text(mFormat.format(DateTime.fromMillisecondsSinceEpoch(int.parse(allNotes[index][DBHelper.COLUMN_NOTE_CREATED_AT]))), style: TextStyle(fontSize: 10),),
+                    ],
+                  ),
                   subtitle: Text(allNotes[index][DBHelper.COLUMN_NOTE_DESC]),
                   trailing: SizedBox(
                     width: 100,
                     child: Row(
                       children: [
+                        ///update
                         IconButton(
                             onPressed: () async{
-                              bool check = await dbHelper.updateNote(
-                                  updatedTitle: "Updated Note",
-                                  updatedDesc: "Updated desc",
-                                  id: allNotes[index][DBHelper.COLUMN_NOTE_ID]);
-
-                              if(check){
-                                getMyNotes();
-                              }
+                              titleController.text = allNotes[index][DBHelper.COLUMN_NOTE_TITLE];
+                              descController.text = allNotes[index][DBHelper.COLUMN_NOTE_DESC];
+                              showModalBottomSheet(context: context, builder: (_){
+                                return getBottomSheetUI(isUpdate: true, id: allNotes[index][DBHelper.COLUMN_NOTE_ID]);
+                              });
 
                             },
                             icon: Icon(Icons.edit)),
+                        ///delete
                         IconButton(
-                            onPressed: () {
-
+                            onPressed: () async{
+                              bool check = await dbHelper.deleteNote(id: allNotes[index][DBHelper.COLUMN_NOTE_ID]);
+                              if(check){
+                                getMyNotes();
+                              }
                             },
                             icon: Icon(
                               Icons.delete,
@@ -75,92 +86,7 @@ class _HomePageState extends State<HomePage> {
                             enableDrag: false,
                             context: context,
                             builder: (_) {
-                              return Container(
-                                width: double.infinity,
-                                padding: EdgeInsets.all(11),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      'Add Note',
-                                      style: TextStyle(
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(
-                                      height: 21,
-                                    ),
-                                    TextField(
-                                      controller: titleController,
-                                      decoration: InputDecoration(
-                                        hintText: "Enter title here..",
-                                        label: Text('Title'),
-                                        enabledBorder: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(11)),
-                                        focusedBorder: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(11)),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 11,
-                                    ),
-                                    TextField(
-                                      controller: descController,
-                                      minLines: 2,
-                                      maxLines: 3,
-                                      decoration: InputDecoration(
-                                        label: Text('Description'),
-                                        hintText: "Enter desc here..",
-                                        enabledBorder: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(11)),
-                                        focusedBorder: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(11)),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 11,
-                                    ),
-                                    Row(
-                                      children: [
-                                        Expanded(
-                                            child: OutlinedButton(
-                                                onPressed: () async {
-                                                  if (titleController
-                                                          .text.isNotEmpty &&
-                                                      descController
-                                                          .text.isNotEmpty) {
-                                                    bool check =
-                                                        await dbHelper.addNote(
-                                                            title:
-                                                                titleController
-                                                                    .text,
-                                                            desc: descController
-                                                                .text);
-
-                                                    if (check) {
-                                                      getMyNotes();
-                                                      Navigator.pop(context);
-                                                    }
-                                                  }
-                                                },
-                                                child: Text('Add'))),
-                                        SizedBox(
-                                          width: 11,
-                                        ),
-                                        Expanded(
-                                            child: OutlinedButton(
-                                                onPressed: () {
-                                                  Navigator.pop(context);
-                                                },
-                                                child: Text('Cancel'))),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              );
+                              return getBottomSheetUI();
                             });
                       },
                       child: Text('Add First Note'))
@@ -177,81 +103,7 @@ class _HomePageState extends State<HomePage> {
                     enableDrag: false,
                     context: context,
                     builder: (_) {
-                      return Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.all(11),
-                        child: Column(
-                          children: [
-                            Text(
-                              'Add Note',
-                              style: TextStyle(
-                                  fontSize: 25, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 21,
-                            ),
-                            TextField(
-                              controller: titleController,
-                              decoration: InputDecoration(
-                                hintText: "Enter title here..",
-                                label: Text('Title'),
-                                enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(11)),
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(11)),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 11,
-                            ),
-                            TextField(
-                              controller: descController,
-                              minLines: 2,
-                              maxLines: 3,
-                              decoration: InputDecoration(
-                                label: Text('Description'),
-                                hintText: "Enter desc here..",
-                                enabledBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(11)),
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(11)),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 11,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: OutlinedButton(
-                                        onPressed: () async {
-                                          if (titleController.text.isNotEmpty &&
-                                              descController.text.isNotEmpty) {
-                                            bool check = await dbHelper.addNote(
-                                                title: titleController.text,
-                                                desc: descController.text);
-
-                                            if (check) {
-                                              getMyNotes();
-                                              Navigator.pop(context);
-                                            }
-                                          }
-                                        },
-                                        child: Text('Add'))),
-                                SizedBox(
-                                  width: 11,
-                                ),
-                                Expanded(
-                                    child: OutlinedButton(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        child: Text('Cancel'))),
-                              ],
-                            ),
-                          ],
-                        ),
-                      );
+                      return getBottomSheetUI();
                     });
               },
               child: Icon(Icons.add),
@@ -265,7 +117,102 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
-/*Widget getBottomSheetUI(){
-    return Container();
-  }*/
+Widget getBottomSheetUI({bool isUpdate = false, int id = 0}){
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(11),
+      child: Column(
+        children: [
+          Text(
+            isUpdate ? 'Update Note' : 'Add Note',
+            style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 21,
+          ),
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(
+              hintText: "Enter title here..",
+              label: Text('Title'),
+              enabledBorder: OutlineInputBorder(
+                  borderRadius:
+                  BorderRadius.circular(11)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius:
+                  BorderRadius.circular(11)),
+            ),
+          ),
+          SizedBox(
+            height: 11,
+          ),
+          TextField(
+            controller: descController,
+            minLines: 2,
+            maxLines: 3,
+            decoration: InputDecoration(
+              label: Text('Description'),
+              hintText: "Enter desc here..",
+              enabledBorder: OutlineInputBorder(
+                  borderRadius:
+                  BorderRadius.circular(11)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius:
+                  BorderRadius.circular(11)),
+            ),
+          ),
+          SizedBox(
+            height: 11,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: OutlinedButton(
+                      onPressed: () async {
+                        if (titleController.text.isNotEmpty &&
+                            descController.text.isNotEmpty) {
+
+                          bool check = false;
+
+                          if(isUpdate){
+                            check = await dbHelper.updateNote(
+                                updatedTitle:
+                                titleController.text,
+                                updatedDesc: descController.text,
+                                updatedAt: DateTime.now().millisecondsSinceEpoch.toString(),
+                                id: id);
+
+                          } else {
+                            check = await dbHelper.addNote(
+                                title: titleController.text,
+                                desc: descController.text,
+                                createdAt: DateTime.now().millisecondsSinceEpoch.toString(),
+                            );
+
+                          }
+
+                          if (check) {
+                            getMyNotes();
+                            Navigator.pop(context);
+                          }
+                        }
+                      },
+                      child: Text(isUpdate ? 'Update' : 'Add'))),
+              SizedBox(
+                width: 11,
+              ),
+              Expanded(
+                  child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancel'))),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
